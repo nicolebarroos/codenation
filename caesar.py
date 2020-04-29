bb53a1df34e0179914b227d50011064f9b58a660
Nicole <barrosnicole094@gmail.com>
2020-04-29 15:02:43 -0300

    atualização

--- caesar.py
+++ caesar.py
@@ -1,26 +1,41 @@
-#!/usr/bin/env python
-# -*- coding:UTF-8 -*-
-#Cifra de César com lista.
-alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-nome = list(input('Digite o nome: '))
-salto = int(input('Valor do salto: '))
-cont = 0
-lista = []
-cifra = []
-while cont < salto: #Insere o valor 0, na posição 0 da lista alfabeto.
-    cont += 1
-    alfabeto.insert(0,0)
-for i in range(len(nome)): #Compara os caracteres inserido com a lista alfabeto atualizada com o salto e armazena o valor na lista.
-    for j in range(len(alfabeto)):
-        if nome[i] == alfabeto[j]:
-            lista.append(j)
-while cont != 0: #Remove os valores 0 inseridos para poder imprimir a cifra.
-    cont -= 1
-    alfabeto.remove(0)
-for n in range(len(lista)):
-    for m in range(len(alfabeto)):
-        if lista[n] > 25: #Se o valor armazenado na lista for maior que 25 ele simplesmente não e identificado, assim a lista se "repete".
-            lista[n] -= 25
-        elif lista[n] == m:
-            cifra.append(alfabeto[m])
-print(''.join(cifra)) #Apresenta a lista como uma string.
+#Codenation Criptografia
+import requests
+import hashlib
+import json
+
+alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+tk = "df296d7c7f81cf5003892f24df518839ccf91708"
+
+urlgit = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={0}'.format(tk)
+response = requests.get(urlgit)
+response_json = response.json()
+numero_casas = response_json['numero_casas']
+cifrado = response_json['cifrado']
+token = response_json['token']
+
+def decifrar():
+    msg = ''
+    for x in cifrado:
+        if x in alfabeto:
+            pos = alfabeto.index(x)
+            msg += alfabeto[pos - numero_casas]
+        else:
+            msg += x
+    return msg.lower()
+
+orig = decifrar()
+resumo = hashlib.sha1(str(orig).encode('utf-8')).hexdigest()
+
+resultado = {
+"numero_casas": numero_casas,
+"token": token,
+"cifrado": cifrado,
+"decifrado": orig,
+"resumo_criptografico": resumo
+}
+
+   
+if __name__ == '__main__':
+    print(resultado)
+    #criar_arquivo()
+    
--- codenation/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def codenation(request):
-
-    #return HttpResponse('Olá Mundo!')
-    return render(request, 'codenation.html')
